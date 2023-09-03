bd01290b68cd50d14b7b57b5e93274540e576228
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-09-03 10:33:23 +0400

    Validators added

---
+++ Pustok/Pustok/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,141 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Pustok.Areas.Admin.ViewModels.Category;
+using Pustok.Database;
+using Pustok.Database.Models;
+using Pustok.Services.Abstracts;
+
+namespace Pustok.Areas.Admin.Controllers;
+
+[Route("admin/categories")]
+[Area("admin")]
+public class CategoryController : Controller
+{
+    private readonly PustokDbContext _dbContext;
+    private readonly IFileService _fileService;
+
+    public CategoryController(PustokDbContext dbContext, IFileService fileService)
+    {
+        _dbContext = dbContext;
+        _fileService = fileService;
+    }
+
+    public async Task<IActionResult> Create([FromBody] Category category)
+    {
+        var validator = new CategoryValidator();
+        var validationResult = await validator.ValidateAsync(category);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(error => error.ErrorMessage);
+            return Json(new { success = false, errors });
+        }
+
+        _dbContext.Categories.Add(category);
+        await _dbContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(Create), new { id = category.Id }, category);
+    }
+
+    #region Index
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var categories = _dbContext.Categories.ToList();
+        var categoryViewModels = categories
+            .Select(c => new CategoryListItemViewModel
+            {
+                Id = c.Id,
+                Name = c.Name
+            })
+            .ToList();
+
+        return View(categoryViewModels);
+    }
+
+    #endregion
+
+    #region Add
+
+    [HttpGet("add")]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(CategoryAddViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View();
+
+        var category = new Category
+        {
+            Name = model.Name,
+        };
+
+        _dbContext.Categories.Add(category);
+        _dbContext.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    #endregion
+
+    #region Update
+
+    [HttpGet("{id}/update")]
+    public IActionResult Update(int id)
+    {
+        var category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+        if (category == null) return NotFound();
+
+        var model = new CategoryUpdateViewModel
+        {
+            Id = category.Id,
+            Name = category.Name
+        };
+
+        return View(model);
+    }
+
+    [HttpPost("{id}/update")]
+    public IActionResult Update(CategoryUpdateViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View();
+
+        var category = _dbContext.Categories.FirstOrDefault(c => c.Id == model.Id);
+        if (category == null)
+        {
+            ModelState.AddModelError("Name", "Category not found");
+            return View();
+        }
+
+        category.Name = model.Name;
+
+        _dbContext.Categories.Update(category);
+        _dbContext.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    #endregion
+
+    #region Delete
+
+    [HttpPost("{id}/delete")]
+    public IActionResult Delete(int id)
+    {
+        var category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+        if (category == null) return NotFound();
+
+        _dbContext.Categories.Remove(category);
+        _dbContext.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    #endregion
+}
---
+++ Pustok/Pustok/Validators/CategoryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Pustok.Database.Models;
+
+public class CategoryValidator : AbstractValidator<Category>
+{
+    public CategoryValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Category Name is required.")
+            .MaximumLength(255).WithMessage("Category Name cannot exceed 255 characters.");
+    }
+}
